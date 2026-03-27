c3f71148be9ff12f6ad9e1d63629c25897d73a75
Wiebke Weber <wiewe372@user.uu.se>
2026-03-28 00:24:44 +0100

    Remove unused utils and config modules

--- qpcr/config.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Thu Mar 26 10:24:20 2026
-
-@author: wiewe372
-"""
-
--- qpcr/utils.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Thu Mar 26 10:24:29 2026
-
-@author: wiewe372
-"""
-
